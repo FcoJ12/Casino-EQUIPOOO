46e2242568060cf9012b6ba1afff091ba5687084
Murat Calderon <Murat@DESKTOP-DFJGCPS>
2024-11-11 21:02:51 -0600

    Ya viene la tragamonedas y la estructura para las interfaces

---
+++ src/casino/Casino.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package casino;
+
+/**
+ *
+ * @author Murat
+ */
+public class Casino {
+    public static void main(String[] args) {
+        CasinoIGU casinoIGU = new CasinoIGU();
+        casinoIGU.setVisible(true);
+    }
+}
