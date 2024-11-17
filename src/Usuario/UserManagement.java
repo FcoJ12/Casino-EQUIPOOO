ec13ef8bdbd63df1045ad0ca8aa84e52608c396e
Meybe <Meybe@MAVIS-01>
2024-11-16 18:15:09 -0600

    logica del manejo de usuarios

---
+++ src/Usuario/UserManagement.java
@@ -0,0 +1,80 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Usuario;
+import java.io.*;
+import java.util.*;
+/**
+ *
+ * @author Meybe
+ */
+public class UserManagement {
+    private static final String FILE_NAME = "Usuarios";
+    private static Map<String, Usuario> usuarios = new HashMap<>();
+    private static Usuario usuarioActual = null;
+    
+    public static String iniciarSesion(){
+        //ingresa el nickname y la contraseña
+        Usuario usuario = usuarios.get(nickname);
+        if(usuario != null && usuario.getContra().equals(contraseña)){
+            usuarioActual = usuario;
+            return "VAMOS A JUGAR";
+        }else{
+            return "Usuaio o contraseña incorrectos.";
+        }
+    }
+    
+    public static String crearUsuario(){
+        //ingresa los datos
+        if(usuarios.containsKey(nickname)){
+            return "El usuario ya existe";
+        }
+        
+        Usuario nuevo = new Usuario(nombre, nickname, contra);
+        usuarios.put(nickname, nuevo);
+        guardarUsuarios();
+    }
+    
+    private static String modificarUsuario() {
+        //ingresa datos a modificar
+        if (!nombre.isEmpty()) usuarioActual.setNombre(nombre);
+
+        if (!contrasena.isEmpty()) usuarioActual.setContrasena(contrasena);
+        
+        guardarUsuarios();
+        
+        return "Perfil actualizado exitosamente.";
+    }
+
+    private static String verUsuarioActual() {
+        return usuarioActual.toString();
+    }
+    
+    private static String guardarUsuarios() {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(FILE_NAME))) {
+            oos.writeObject(usuarios);
+            return "Usuarios guardados exitosamente.";
+        } catch (IOException e) {
+            String a = "Error al guardar usuarios: " + e.getMessage();
+            return a;
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private static String cargarUsuarios() {
+        File file = new File(FILE_NAME);
+        if (file.exists()) {
+            try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file))) {
+                usuarios = (Map<String, Usuario>) ois.readObject();
+                return "Usuarios cargados exitosamente.";
+            } catch (IOException | ClassNotFoundException e) {
+                String a = "Error al cargar usuarios: " + e.getMessage();
+                return a;
+            }
+        } else {
+            return "El archivo no existe. Se creará un nuevo mapa de usuarios.";
+        }
+    }
+}
+    
